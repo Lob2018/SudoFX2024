946af9b427174adbbd687c3fbd71c58edeb56aa6
Lob <37583137+Lob2018@users.noreply.github.com>
2024-05-30 11:50:16 +0200

    Feat : Spring

--- src/main/java/fr/softsf/sudofx2024/model/BackgroundModel.java
+++ src/main/java/fr/softsf/sudofx2024/model/BackgroundModel.java
@@ -1,6 +1,7 @@
 package fr.softsf.sudofx2024.model;
 
 import jakarta.persistence.*;
+
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
