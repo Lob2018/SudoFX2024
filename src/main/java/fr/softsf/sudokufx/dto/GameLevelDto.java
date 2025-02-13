d5175efe3274cfc25b42acf53a2d7c97233d7982
Lob <37583137+Lob2018@users.noreply.github.com>
2025-02-13 20:03:42 +0100

    Fix GameLevel : Removed unused imports

--- src/main/java/fr/softsf/sudokufx/dto/GameLevelDto.java
+++ src/main/java/fr/softsf/sudokufx/dto/GameLevelDto.java
@@ -3,7 +3,6 @@ package fr.softsf.sudokufx.dto;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 
 public record GameLevelDto(
         Byte levelid,
--- src/main/java/fr/softsf/sudokufx/model/GameLevel.java
+++ src/main/java/fr/softsf/sudokufx/model/GameLevel.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Entity
