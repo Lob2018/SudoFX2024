8334ebf5ac9810742f11ce7a41d2449e78b238c1
Lob2018 <@>
2025-03-05 14:01:28 +0100

    Doc DOXYGEN

--- docs/classfr_1_1softsf_1_1sudokufx_1_1_sudo_main.js
+++ docs/classfr_1_1softsf_1_1sudokufx_1_1_sudo_main.js
@@ -1,6 +1,5 @@
 var classfr_1_1softsf_1_1sudokufx_1_1_sudo_main =
 [
-    [ "createSpringContextTask", "classfr_1_1softsf_1_1sudokufx_1_1_sudo_main.html#ad38d74b9ba975c931a8f6aa3e3e45c73", null ],
     [ "getPauseTransition", "classfr_1_1softsf_1_1sudokufx_1_1_sudo_main.html#a5f9601381fd0f35bc4a5c90205958aec", null ],
     [ "handleSpringContextTaskFailed", "classfr_1_1softsf_1_1sudokufx_1_1_sudo_main.html#a5f0246399e65a32405abe1ed415219ee", null ],
     [ "handleSpringContextTaskSuccess", "classfr_1_1softsf_1_1sudokufx_1_1_sudo_main.html#afa5bb361c3c4da3cef227f656f09c3e3", null ],
@@ -13,5 +12,6 @@ var classfr_1_1softsf_1_1sudokufx_1_1_sudo_main =
     [ "fxmlService", "classfr_1_1softsf_1_1sudokufx_1_1_sudo_main.html#a157bd6259c6b80ac66f52c80860d82e1", null ],
     [ "iMainStageView", "classfr_1_1softsf_1_1sudokufx_1_1_sudo_main.html#adc6ce5bc2ff2ef8936339a0ff7af50b5", null ],
     [ "isplashScreenView", "classfr_1_1softsf_1_1sudokufx_1_1_sudo_main.html#a23e016b1db2d39f8b2ab09c12afcd958", null ],
-    [ "scene", "classfr_1_1softsf_1_1sudokufx_1_1_sudo_main.html#acd04137840e8c91a591b1401685084ee", null ]
+    [ "scene", "classfr_1_1softsf_1_1sudokufx_1_1_sudo_main.html#acd04137840e8c91a591b1401685084ee", null ],
+    [ "springContextTask", "classfr_1_1softsf_1_1sudokufx_1_1_sudo_main.html#a7a44ca01808ecbb7aec4a4ab17436d87", null ]
 ];
